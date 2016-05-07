847059a068343a7ae4e6ed2003abe189a0b91c5d
jaynarol <jaynarol@gmail.com>
2016-05-07 19:46:15 +0700

    fix path

--- cmd/pack.sh
+++ cmd/pack.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker-compose up -d
-docker exec mysql sh -c 'exec mysqldump -uroot -p"$MYSQL_ROOT_PASSWORD" --databases world' > ./build/sql/world.sql
+docker exec mysql sh -c 'exec mysqldump -uroot -p"$MYSQL_ROOT_PASSWORD" --databases world' > ./build/mysql/world.sql
 docker-compose down
 rm -rf ./tmp
