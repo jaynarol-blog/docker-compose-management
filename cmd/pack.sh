39ea9bb9b3460a1aa544443825d6a3393b1a9e4f
jaynarol <jaynarol@gmail.com>
2016-05-08 16:06:34 +0700

    improve performance

---
+++ cmd/pack.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker-compose up -d
+docker exec mysql sh -c 'exec mysqldump -uroot -p"$MYSQL_ROOT_PASSWORD" --databases world' | gzip -9 > ./build/mysql/world.sql.gz
+docker-compose down
+rm -rf ./tmp/mysql
