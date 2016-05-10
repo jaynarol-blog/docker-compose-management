c9f8c2f3f7de0f674a8bcecb9d7201590d687425
jaynarol <jaynarol@gmail.com>
2016-05-10 23:41:00 +0700

    upzip sql file

--- cmd/pack.sh
+++ cmd/pack.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker-compose up -d
-docker exec mysql sh -c 'exec mysqldump -uroot -p"$MYSQL_ROOT_PASSWORD" --databases world' | gzip -9 > ./build/mysql/world.sql.gz
+docker exec mysql sh -c 'exec mysqldump -uroot -p"$MYSQL_ROOT_PASSWORD" --databases world' > ./build/mysql/world.sql.gz
