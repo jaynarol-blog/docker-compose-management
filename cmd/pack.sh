59225e05670c0bbed57c95f184a8b4eee04583b8
jaynarol <jaynarol@gmail.com>
2016-05-10 22:25:57 +0700

    add clean

--- cmd/pack.sh
+++ cmd/pack.sh
@@ -2,5 +2,3 @@
 
 docker-compose up -d
 docker exec mysql sh -c 'exec mysqldump -uroot -p"$MYSQL_ROOT_PASSWORD" --databases world' | gzip -9 > ./build/mysql/world.sql.gz
-docker-compose down
-rm -rf ./tmp/mysql
